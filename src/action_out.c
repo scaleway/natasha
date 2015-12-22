6dd312d890bf50700f9d2979a05385b705b00026
Julien Castets <jcastets@scaleway.com>
2015-12-22 14:58:17 +0000

    Send packets in action_out

--- src/action_out.c
+++ src/action_out.c
@@ -16,10 +16,13 @@ action_out(struct rte_mbuf *pkt, uint8_t port, struct core *core, void *data)
     rte_eth_macaddr_get(port, &eth_hdr->s_addr);
     ether_addr_copy(&out->next_hop, &eth_hdr->d_addr);
 
-    // recompute ipv4 header
+    // Offloat IPv4 checksum
+    ipv4_header(pkt)->hdr_checksum = 0;
+    pkt->ol_flags |= PKT_TX_IP_CKSUM;
+
     // recompute udp header if udp
     // recompute tcp header if tcp
 
-    // tx_send(pkt, out->port, &core->tx_queues[out->port]);
+    tx_send(pkt, out->port, &core->tx_queues[out->port]);
     return ACTION_BREAK;
 }

76922d554f2e86fe4033d13a828590c52108e85a
Julien Castets <jcastets@scaleway.com>
2016-04-08 15:19:04 +0000

    Reduce descriptors to improve perfs

--- src/core.c
+++ src/core.c
@@ -253,8 +253,8 @@ port_init(uint8_t port, struct app_config *app_config, struct core *cores)
     /* Configure queues */
     queue_id = 0;
     RTE_LCORE_FOREACH_SLAVE(core) {
-        static const int rx_ring_size = 1024;
-        static const int tx_ring_size = 4096;
+        static const int rx_ring_size = 256;
+        static const int tx_ring_size = 512;
 
         static const struct rte_eth_rxconf rx_conf = {
             .rx_thresh = {

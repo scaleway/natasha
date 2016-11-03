b87069d12230f4d4952acae1d4c9c095d2ea5875
Julien Castets <jcastets@scaleway.com>
2016-11-03 10:39:46 +0100

    Fix indentation

--- src/core.c
+++ src/core.c
@@ -441,7 +441,7 @@ natasha(int argc, char **argv)
 
     ret = rte_eal_init(argc, argv);
     if (ret < 0) {
-		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
+        rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
     }
     argc -= ret;
     argv += ret;

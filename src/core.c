scaleway/natasha

commit bfbf3a6be0ba2122f968b1cb34e26c346032b708

core: add signal handler to properly exit app
When supervisord stops natasha, it sends SIGTERM signal to the master
core, let's register this signal and set a hundler to stop slaves and close
ports.

Signed-off-by: Amine Kherbouche <akherbouche@scaleway.com>

src/core.c
@@ -1,5 +1,8 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include <rte_common.h>
 #include <rte_cycles.h>
@@ -117,7 +120,7 @@ main_loop(void *pcore)
 
     eth_dev_count = rte_eth_dev_count();
 
-    while (1) {
+    while (!force_quit) {
         // At any time, config.c/app_config_reload_all() can update
         // core->app_config to load a new configuration. The reload function
         // needs to free the old configuration, and for that it waits us to
@@ -562,6 +565,43 @@ setup_app(struct core *cores, int argc, char **argv)
     return 0;
 }
 
+void
+natasha_exit()
+{
+    uint8_t eth_dev_count;
+    uint8_t portid;
+
+    eth_dev_count = rte_eth_dev_count();
+    if (!eth_dev_count) {
+        RTE_LOG(ERR, APP, "No network device using DPDK-compatible driver\n");
+        return;
+    }
+
+    for (portid = 0; portid < eth_dev_count; ++portid) {
+        RTE_LOG(INFO, APP, "Stopping port %i...\n", portid);
+        rte_eth_dev_stop(portid);
+        rte_eth_dev_close(portid);
+    }
+    /* TODO: free app configuration, TBD when changing app conf structures */
+}
+
+static void
+sigterm_handler(int signum)
+{
+    if (signum != SIGTERM) {
+        RTE_LOG(WARNING, APP, "Received signal %s cannot be handled\n",
+                strsignal(signum));
+        return;
+    }
+
+    RTE_LOG(WARNING, APP, "Received signal %s, preparing to exit...\n",
+            strsignal(signum));
+    force_quit = true;
+    natasha_exit();
+    signal(signum, SIG_DFL);
+    kill(getpid(), signum);
+}
+
 int
 #ifndef UNITTEST
 main(int argc, char **argv)
@@ -586,6 +626,8 @@ natasha(int argc, char **argv)
         rte_exit(EXIT_FAILURE, "Unable to set stderr as non-blocking\n");
     }
 
+    force_quit = false;
+    signal(SIGTERM, sigterm_handler);
     ret = rte_eal_init(argc, argv);
     if (ret < 0) {
         rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");

src/natasha.h
@@ -1,6 +1,8 @@
 #ifndef CORE_H_
 #define CORE_H_
 
+#include <stdbool.h>
+
 #include <rte_ethdev.h>
 #include <rte_rwlock.h>
 #include <rte_malloc.h>
@@ -25,6 +27,9 @@
 /*
  * Application configuration.
  */
+
+/* Variable used to stop slaves mainloop */
+volatile bool force_quit;
 
 // Forward declaration. Defined under "Workers and queues configuration".
 struct core;

commit 47162e7a3b74075243146d42756f6c725938b884

core: wait for all lcores before closing ports
When a SIGTERM is received we must wait for all workers to stop properly
before closing the port device. This may cause a firmware issue.

Fixes: bfbf3a6be0ba (core: add signal handler to properly exit app)
Signed-off-by: Amine Kherbouche <akherbouche@scaleway.com>

src/core.c
@@ -571,23 +571,30 @@ setup_app(struct core *cores, int argc, char **argv)
 void
 natasha_exit()
 {
-    uint8_t eth_dev_count;
-    uint8_t portid;
+    uint32_t lcore_id;
+    uint16_t portid;
 
-    eth_dev_count = rte_eth_dev_count();
-    if (!eth_dev_count) {
-        RTE_LOG(ERR, APP, "No network device using DPDK-compatible driver\n");
-        return;
+    /* stop and wait for slaves */
+    force_quit = true;
+    RTE_LCORE_FOREACH_SLAVE(lcore_id) {
+        if (rte_eal_wait_lcore(lcore_id) < 0)
+            RTE_LOG(WARNING, APP, "Core %d exited with failure !\n", lcore_id);
+        else
+            RTE_LOG(INFO, APP, "Core %d successfuly stoped.\n", lcore_id);
     }
 #ifdef RTE_LIBRTE_PDUMP
         rte_pdump_uninit();
 #endif
-    for (portid = 0; portid < eth_dev_count; ++portid) {
-        RTE_LOG(INFO, APP, "Stopping port %i...\n", portid);
+    RTE_ETH_FOREACH_DEV(portid) {
+        RTE_LOG(INFO, APP, "Stopping port %i...", portid);
         rte_eth_dev_stop(portid);
         rte_eth_dev_close(portid);
+        RTE_LOG(INFO, APP, "Done\n");
     }
-    /* TODO: free app configuration, TBD when changing app conf structures */
+    /*
+     * TODO:
+     * free app configuration, TBD when reworking app conf structures
+     */
 }
 
 static void
@@ -601,7 +608,6 @@ sigterm_handler(int signum)
 
     RTE_LOG(WARNING, APP, "Received signal %s, preparing to exit...\n",
             strsignal(signum));
-    force_quit = true;
     natasha_exit();
     signal(signum, SIG_DFL);
     kill(getpid(), signum);
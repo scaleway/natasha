scaleway/natasha

commit d24811068f935bf70a7fe2ee135102733ff3bc29

recompute properly the udp csum for the first frag only and don't recompute icmp csum for frags
(cherry picked from commit 8cb524dd7ff752fad6c52b1a361bb2c6ff5bf80b)

src/action_nat.c
@@ -107,6 +107,7 @@ action_nat_rewrite_impl(struct rte_mbuf *pkt, uint8_t port, struct core *core,
     struct icmp_hdr *icmp_hdr;
     struct ipv4_hdr *inner_ipv4_hdr;
     uint32_t *inner_ipv4_address;
+    uint32_t save_ipv4 = *address;
 
     // Rewrite IPv4 source or destination address.
     if (lookup_and_rewrite(pkt,
@@ -117,6 +118,19 @@ action_nat_rewrite_impl(struct rte_mbuf *pkt, uint8_t port, struct core *core,
     // processing rules for this packet (which has been freed by
     // lookup_and_rewrite()).
         return -1;
+    }
+
+    // Test is we have a fragmented packet and if it's the firest fragment
+    //if (unlikely(NATA_FIRST_FRAG(ipv4_hdr) && ipv4_hdr.next_proto_id == IPPROTO_UDP)) {
+    if (unlikely(NATA_FIRST_FRAG(ipv4_hdr))) {
+        if (ipv4_hdr->next_proto_id == IPPROTO_UDP) {
+            struct udp_hdr *udp_hdr = udp_header(pkt);
+            // recalculate the csum
+            udp_hdr->dgram_cksum -= save_ipv4 & 0xffff;
+            udp_hdr->dgram_cksum -= save_ipv4>>16 & 0xffff;
+            udp_hdr->dgram_cksum += *address & 0xffff;
+            udp_hdr->dgram_cksum += *address>>16 & 0xffff;
+        }
     }
 
     // pkt is probably not an ICMP packet, there is no need to handle it

src/network_headers.h
@@ -47,6 +47,12 @@ L4_HEADER(udp, struct udp_hdr);
 #undef L2_HEADER
 #undef L3_HEADER
 
+#define NATA_FIRST_FRAG(ipv4_hdr) \
+    (((ipv4_hdr)->fragment_offset & (IPV4_HDR_MF_FLAG + IPV4_HDR_OFFSET_MASK)) == htons(IPV4_HDR_MF_FLAG))
+
+#define NATA_IS_FRAG(ipv4_hdr) \
+    (((ipv4_hdr)->fragment_offset & (IPV4_HDR_MF_FLAG + IPV4_HDR_OFFSET_MASK)) != 0)
+
 // last 12 bits of the TCI field
 #define VLAN_ID(pkt) ((pkt)->vlan_tci & 0xfff)
 

src/action_out.c
@@ -43,6 +43,9 @@ action_out(struct rte_mbuf *pkt, uint8_t port, struct core *core, void *data)
     case IPPROTO_UDP: {
         struct udp_hdr *udp_hdr;
 
+        // Frags csum are calculated during action_nat_rewrite_impl()
+        if (unlikely(NATA_IS_FRAG(ipv4_hdr)))
+            break;
         udp_hdr = udp_header(pkt);
         udp_hdr->dgram_cksum = 0;
         pkt->ol_flags |= PKT_TX_UDP_CKSUM;
@@ -52,6 +55,9 @@ action_out(struct rte_mbuf *pkt, uint8_t port, struct core *core, void *data)
     case IPPROTO_ICMP: {
         struct icmp_hdr *icmp_hdr;
 
+        // Frags csum are calculated during action_nat_rewrite_impl()
+        if (unlikely(NATA_IS_FRAG(ipv4_hdr)))
+            break;
         icmp_hdr = icmp_header(pkt);
         icmp_hdr->icmp_cksum = 0;
         // ICMP checksum can't be offloaded.

commit 9dffa16b5d6ab58194ec0131e736d0005bb5fa9d

ip: fix checking fragments
The bit mask used to check the flag MF was not correct and any bit set
to 1 in the offset field in the packet was detected as a fragmented
packet.

NATA_IS_FIRST_FRAG macro rework now checks the flag MF and the offset to
0 to determine whether it's indeed the first fragment.

Fixes: d24811068f93 (recompute properly the udp csum for the first frag
only and don't recompute icmp csum for frags)
Signed-off-by: Amine Kherbouche <akherbouche@scaleway.com>

src/action_nat.c
@@ -111,7 +111,7 @@ action_nat_rewrite_impl(struct rte_mbuf *pkt, uint8_t port, struct core *core,
 
     // Test is we have a fragmented packet and if it's the firest fragment
     //if (unlikely(NATA_FIRST_FRAG(ipv4_hdr) && ipv4_hdr.next_proto_id == IPPROTO_UDP)) {
-    if (unlikely(NATA_FIRST_FRAG(ipv4_hdr))) {
+    if (unlikely(NATA_IS_FIRST_FRAG(ipv4_hdr))) {
         if (ipv4_hdr->next_proto_id == IPPROTO_UDP) {
             struct udp_hdr *udp_hdr = udp_header(pkt);
             // recalculate the csum

src/network_headers.h
@@ -49,11 +49,13 @@ L4_HEADER(udp, struct udp_hdr);
 #undef L2_HEADER
 #undef L3_HEADER
 
-#define NATA_FIRST_FRAG(ipv4_hdr) \
-    (((ipv4_hdr)->fragment_offset & (IPV4_HDR_MF_FLAG + IPV4_HDR_OFFSET_MASK)) == htons(IPV4_HDR_MF_FLAG))
+/* check if it's the first fragment by having MF flag and offset set to 0 */
+#define NATA_IS_FIRST_FRAG(ipv4_hdr)                                \
+    (((ipv4_hdr)->fragment_offset & htons(IPV4_HDR_MF_FLAG)) &&     \
+     !((ipv4_hdr)->fragment_offset & htons(IPV4_HDR_OFFSET_MASK)))
 
 #define NATA_IS_FRAG(ipv4_hdr) \
-    (((ipv4_hdr)->fragment_offset & (IPV4_HDR_MF_FLAG + IPV4_HDR_OFFSET_MASK)) != 0)
+    (((ipv4_hdr)->fragment_offset & htons(IPV4_HDR_MF_FLAG)))
 
 // last 12 bits of the TCI field
 #define VLAN_ID(pkt) ((pkt)->vlan_tci & 0xfff)